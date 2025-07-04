410442a7ceaa1f847b986d64ac079d1f7af27e1e
Mahmoud Ismail <mahmoudismailkn@gmail.com>
2025-07-04 17:23:57 +0300

    Update TX.

--- TX.v
+++ TX.v
@@ -2,4 +2,10 @@ module TX (input clk, rst, par_typ, par_en, data_valid,
     input [7:0] p_data, 
     output reg tx_out, busy);
 
+integer i;
+reg [7:0] data_container;
+
+always @ (posedge clk) begin
+
+
 endmodule
