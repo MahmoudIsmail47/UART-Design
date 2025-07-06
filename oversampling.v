9d1d2c406ea46bd45275139e8b6a219d5e0924b0
Mahmoud Ismail <mahmoudismailkn@gmail.com>
2025-07-06 18:19:34 +0300

    Oversampling block is finished.

---
+++ oversampling.v
@@ -0,0 +1,40 @@
+module CLK_DIV (input clk, prescale, output reg clk_out);
+
+integer i;
+reg flare;
+
+always @ (*) begin
+    if (prescale) begin
+        flare = 1;
+        if (i%4 && i < 4) begin
+            clk_out = 1'b1;
+        end
+        else if (i%7) begin
+            clk_out = 1'b0;
+            if (i == 6) begin
+                flare = 0;
+            end
+            else begin
+                flare = 1;
+            end
+        end
+        else begin
+            flare = 0;
+            clk_out = clk;
+        end
+    end
+    else begin
+        flare = 0;
+        clk_out = clk;
+    end
+end
+
+always @ (posedge clk) begin
+    if (flare) begin
+        i = i + 1;
+    end
+    else begin
+        i = 1;
+    end
+end
+endmodule
